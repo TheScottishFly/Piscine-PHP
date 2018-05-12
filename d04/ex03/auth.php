c1e904f6c08d10cb8d506c7dd295a611d3fe04ef
Gilian Rosnet boutinet <grosnet-@z3r1p4.le-101.fr>
2018-05-12 21:55:25 +0200

    d04 ex03

---
+++ d04/ex03/auth.php
@@ -0,0 +1,12 @@
+<?php
+    function auth($login, $passwd)
+    {
+        $a = unserialize(file_get_contents("../private/passwd"));
+        $password = hash("sha256", $passwd);
+        foreach ($a as $user)
+        {
+            if ($user["login"] == $login && $user["passwd"] == $password)
+                return TRUE;
+        }
+        return FALSE;
+    }
---
+++ d04/ex03/login.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+    include("auth.php");
+    $login = $_GET["login"];
+    $passwd = $_GET["passwd"];
+    if (auth($login, $passwd) == TRUE)
+    {
+    	$_SESSION["loggued_on_user"] = $login;
+        echo "OK\n";
+    }
+    else
+    {
+    	$_SESSION["loggued_on_user"] = "";
+        exit("ERROR\n");
+    }
