ef840508be6bc108102b55eade329de079b9784d
Gilian Rosnet boutinet <grosnet-@z3r2p3.le-101.fr>
2018-05-15 21:24:34 +0200

    ex11

--- d01/ex11/do_op_2.php
+++ d01/ex11/do_op_2.php
@@ -1,31 +1,30 @@
 #!/usr/bin/php
 <?php
-if ($argc == 2)
-{
-	unset($argv[0]);
-	$argv[1] = trim($argv[1]);
-	$tab = preg_split('/(-|\/|%|\*|\+|\s+)/', $argv[1], -1, PREG_SPLIT_NO_EMPTY);
-	$err = 0;
-	$argv[1] = preg_replace('/\s+/', "", $argv[1]);
-	for ($i=0; $i < strlen($argv[1]); $i++)
-		if (!(is_numeric($argv[1][$i])))
-			$err += 1;
-	if (count($tab) != 2 || $err != 1)
-		exit ("Syntax Error\n");
-	if (strchr($argv[1], "+"))
-		$nbr = $tab[0] + $tab[1];
-	else if (strchr($argv[1], "-"))
-		$nbr = $tab[0] - $tab[1];
-	else if (strchr($argv[1], "*"))
-		$nbr = $tab[0] * $tab[1];
-	else if (strchr($argv[1], "/"))
-		$nbr = $tab[0] / $tab[1];
-	else if (strchr($argv[1], "%"))
-		$nbr = $tab[0] % $tab[1];
-	else
-		exit ("Syntax Error\n");
-	echo "$nbr\n";
+if ($argc != 2)
+	exit("Incorrect Parameters\n");
+if (!preg_match("#^\s*([+-]?[0-9]+)\s*([\+\-\*\/\%])\s*([+-]?[0-9]+)\s*$#", $argv[1], $match))
+	exit("Syntax Error\n");
+$op = $match[2];
+$num1 = $match[1];
+$num2 = $match[3];
+switch ($op) {
+case ("*") :
+	echo $num1 * $num2;
+	break;
+case ("+") :
+	echo $num1 + $num2;
+	break;
+case ("-") :
+	echo $num1 - $num2;
+	break;
+case ("/") :
+	echo $num1 / $num2;
+	break;
+case ("%") :
+	echo $num1 % $num2;
+	break;
+default:
+	echo "Syntax Error\n";
+	exit();
 }
-else
-	echo "Incorrect Parameters\n";
-?>
+echo "\n";
--- d01/ex12/search_it!.php
+++ d01/ex12/search_it!.php
@@ -5,13 +5,10 @@
 		$v = $argv[1];
 		$a = array();
 		while ($argv[$i]) {
-			$arg = explode(":", trim($argv[$i]));
-			if (count($arg) == 2)
-			{
-				$key = trim($arg[0]);
-				$value = trim($arg[1]);
-				$a[$key] = $value;
-			}
+			$arg = explode(":", $argv[$i]);
+			$key = $arg[0];
+			$value = $arg[1];
+			$a[$key] = $value;
 			$i += 1;
 		}
 		if (array_key_exists($v, $a))
