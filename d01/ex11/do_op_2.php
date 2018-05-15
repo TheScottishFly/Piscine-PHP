7dd03eeec619ff29d195b9bd8667fe6ed2aefac4
Gilian Rosnet boutinet <grosnet-@z3r2p3.le-101.fr>
2018-05-15 08:58:12 +0200

    d01

--- d01/ex11/do_op_2.php
+++ d01/ex11/do_op_2.php
@@ -1,38 +1,51 @@
 #!/usr/bin/php
 <?php
-	if ($argc == 2)
+	function find_op($str){
+		for ($i = 0; $i < strlen($str); $i++)
+		{
+			if ($str[$i] == "+")
+				return 1;
+			if ($str[$i] == "-")
+				return 2;
+			if ($str[$i] == "*")
+				return 3;
+			if ($str[$i] == "/")
+				return 4;
+			if ($str[$i] == "%")
+				return 5;
+			$i++;
+		}
+		return (0);
+	}
+	
+	if ($argc != 2){
+		echo "Incorrect Parameters\n";
+		exit();
+	}
+	$argv[1] = preg_replace('/\s+/', '', $argv[1]);
+	$op = find_op($argv[1]);
+	if ($op == 0)
 	{
-		$args = explode(" ", $argv[1]);
-		$i = 0;
-		foreach($args as $a) {
-			if (strlen($a) == 0)
-				unset($args[$i]);
-			$i += 1;
-		}
-		$args = array_values(array_filter($args));
-		if (count($args) != 3)
-			echo "Incorrect Parameters\n";
-		else {
-			$o1 = trim($args[0]);
-			$op = trim($args[1]);
-			$o2 = trim($args[2]);
-			if (!in_array($op, array("+", "-", "/", "%", "*")))
-				echo "Incorrect Parameters\n";
-			else {
-				if ($op == "/")
-					$ret = $o1 / $o2;
-				elseif ($op == "*")
-					$ret = $o1 * $o2;
-				elseif ($op == '-')
-					$ret = $o1 - $o2;
-				elseif ($op == "%")
-					$ret = $o1 % $o2;
-				elseif ($op == '+')
-					$ret = $o1 + $o2;
-				echo $ret . "\n";
-			}
-		}
+		echo "Syntax Error\n";
+		exit();
+	}
+	$arr = preg_split("/(-|\/|%|\*|\+)/", $argv[1], -1, PREG_SPLIT_NO_EMPTY);
+	if(is_numeric($arr[0]) && is_numeric($arr[1]))
+	{
+		if ($op == 1)
+			$res = $arr[0] + $arr[1];
+		else if ($op == 2)
+			$res = $arr[0] - $arr[1];
+		else if ($op == 3)
+			$res = $arr[0] * $arr[1];
+		else if ($op == 4)
+			$res = $arr[0] / $arr[1];
+		else if ($op == 5)
+			$res = $arr[0] % $arr[1];
+		else
+			echo "Syntax Error\n";
+		echo "$res\n";
 	}
 	else
-		echo "Incorrect Parameters\n";
+		echo "Syntax Error\n";
 ?>
