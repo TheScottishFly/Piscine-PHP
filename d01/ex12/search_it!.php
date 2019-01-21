c18f2f4f8971fd3edb8e4c5a354c6c2acff2428f
Gilian Rosnet boutinet <grosnet-@z4r3p1.le-101.fr>
2019-01-21 09:01:02 +0100

    fix d01 ex12

--- d01/ex12/search_it!.php
+++ d01/ex12/search_it!.php
@@ -2,16 +2,17 @@
 <?php
 	if ($argc > 0) {
 		$i = 2;
-		$v = $argv[1];
+		if (isset($argv[1]))
+    		$v = $argv[1];
 		$a = array();
-		while ($argv[$i]) {
+		while (isset($argv[$i])) {
 			$arg = explode(":", $argv[$i]);
 			$key = $arg[0];
 			$value = $arg[1];
 			$a[$key] = $value;
 			$i += 1;
 		}
-		if (array_key_exists($v, $a))
+		if (isset($v) && array_key_exists($v, $a))
 			echo $a[$v] . "\n";
 	}
 ?>
