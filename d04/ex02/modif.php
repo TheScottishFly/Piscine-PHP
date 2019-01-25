7b3ff9bc0f9f666776662bbc7de5c73b3f7daeff
Gilian Rosnet boutinet <grosnet-@z4r3p1.le-101.fr>
2019-01-25 09:40:44 +0100

    fix d04 ex02

--- d04/ex02/modif.php
+++ d04/ex02/modif.php
@@ -1,16 +1,16 @@
 <?php
-    if (!file_exists("../private") || !file_exists("../private/passwd") || $_POST["login"] === "" || $_POST["oldpw"] === "" || $_POST["newpw"] === "" || $_POST["submit"] !== "OK")
+    if (!file_exists("../private/passwd") || !isset($_POST["login"]) || !isset($_POST["passwd"]) || !isset($_POST["submit"]) || $_POST["login"] === "" || $_POST["oldpw"] === "" || $_POST["newpw"] === "" || $_POST["submit"] !== "OK")
     	exit("ERROR\n");
 
     $a = unserialize(file_get_contents("../private/passwd"));
     $login = $_POST["login"];
-    $oldpw = hash("sha256", $_POST["oldpw"]);
-    $newpw = hash("sha256", $_POST["newpw"]);
-    foreach ($a as $user)
+    $oldpw = hash("sha512", $_POST["oldpw"]);
+    $newpw = hash("sha512", $_POST["newpw"]);
+    foreach ($a as $key => $user)
     {
-        if ($user["login"] === $login && $user["passwd"] === $oldpw && $oldpw !== $newpw)
+        if ($user["login"] === $login && $user["passwd"] === $oldpw)
         {
-            $user["passwd"] = $newpw;
+            $a[$key]["passwd"] = $newpw;
             file_put_contents("../private/passwd", serialize($a));
             exit("OK\n");
         }
